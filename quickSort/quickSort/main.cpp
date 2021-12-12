08112320ddc90ee253d7c83559099d48d3be4adf
easy297 <93608846+easy297@users.noreply.github.com>
2021-12-12 10:37:30 +0600

    heapSort

--- quickSort/quickSort/main.cpp
+++ quickSort/quickSort/main.cpp
@@ -6,9 +6,58 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+static void swap(int* arr, int x, int y)
+{
+int temp = arr[x];
+arr[x] = arr[y];
+arr[y] = temp;
+}
+
+static int partition(int* arr, int p, int r)
+{
+    int i = p - 1, k = p;
+    while(k < r)
+    {
+        if(arr[k] < arr[r])
+        {
+            swap(arr, k, i + 1);
+            i++;
+        }
+        k++;
+    }
+    swap(arr, i + 1, r);
+    return i + 1;
+}
+
+static void quickSort(int* arr, int p, int r)
+{
+    if(p < r)
+    {
+        int q = partition(arr, p, r);
+        quickSort(arr, p, q - 1);
+        quickSort(arr, q + 1, r);
+    }
+}
+
+int main() {
+    int p = 0;
+    int n;
+    std::cout << "Enter your array's N: ";
+    std::cin >> n;
+    int r = n - 1;
+    int* arr = new int[n];
+    std::cout << "Fulfill your array: ";
+    for (int i = 0; i < n; i++)
+    {
+    std::cin >> arr[i];
+    }
+
+    quickSort(arr, p, r);
+    std::cout << "Here's your array: ";
+    for (int i = 0; i < n; i++)
+    {
+    std::cout << arr[i] << " ";
+    }
 }
